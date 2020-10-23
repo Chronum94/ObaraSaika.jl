dc54eb35de53ebc26c250b323d59c07ad7db5ade
Eric Berquist <eric.berquist@gmail.com>
2020-10-23 00:30:14 -0400

    Put existing code into PkgTemplate structure

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,29 @@ using ObaraSaika
 using Test
 
 @testset "ObaraSaika.jl" begin
-    # Write your tests here.
+    @test ObaraSaika.get_ijk_list(0) == Array{Int, 1}[[0,0,0]]
+    @test ObaraSaika.get_ijk_list(1) == Array{Int, 1}[[1,0,0],[0,1,0],[0,0,1]]
+    @test ObaraSaika.get_ijk_list(2) == Array{Int, 1}[[2,0,0],[1,1,0],[1,0,1],[0,2,0],[0,1,1],[0,0,2]]
+
+    @test ObaraSaika.get_shell2(0, 0) == Array{Int, 1}[
+        [0, 0, 0, 0, 0, 0]
+    ]
+    @test ObaraSaika.get_shell2(0, 1) == Array{Int, 1}[
+        [0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 1]
+    ]
+    @test ObaraSaika.get_shell2(1, 0) == Array{Int, 1}[
+        [1, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0]
+    ]
+    @test ObaraSaika.get_shell2(0, 2) == Array{Int, 1}[
+        [0, 0, 0, 2, 0, 0],
+        [0, 0, 0, 1, 1, 0],
+        [0, 0, 0, 1, 0, 1],
+        [0, 0, 0, 0, 2, 0],
+        [0, 0, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0, 2]
+    ]
 end
